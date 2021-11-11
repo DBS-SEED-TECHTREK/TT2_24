eeb010bcfb2ca5886242bb757a518daa5a889e0b
ArunBeCoding <arunkumarr97@gmail.com>
2021-11-11 11:28:34 +0800

    frontend initial commit

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask import request
+from flask import Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/login', methods=['GET'])
+def login():
+    username = request.authorization.username
+    password = request.authorization.password
+
+    # CHECK USERNAME & PASSWORD
+
+    # CREATE TOKEN
+
+    return Response(status=201)
+
